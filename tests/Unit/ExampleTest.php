fc6ebeb0940ba992096ba441517420784c1b69cc
JorikDura <vandarkslave@outlook.com>
2024-07-06 23:13:09 +0300

    removed unit test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,22 +1,8 @@
 <?php
 
-use App\Models\User;
 
-use function Pest\Laravel\assertDatabaseHas;
 use function PHPUnit\Framework\assertTrue;
 
-it('base test', function () {
+it('unit test example', function () {
     assertTrue(true);
-    User::create([
-        'name' => 'test',
-        'email' => 'test@test.com',
-        'password' => 'test',
-    ]);
-    assertDatabaseHas(
-        table: 'users',
-        data: [
-            'name' => 'test',
-            'email' => 'test@test.com'
-        ]
-    );
 });
